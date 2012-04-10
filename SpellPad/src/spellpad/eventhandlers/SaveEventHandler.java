6ec41640afc0252aff0ce515bb73e6485ce9e798
Jesse <Jesse@Work-PC>
2012-04-10 10:25:17 -0600

    Day 2, Part 1: Finished day 6: Saving.

--- SpellPad/src/spellpad/eventhandlers/SaveEventHandler.java
+++ SpellPad/src/spellpad/eventhandlers/SaveEventHandler.java
@@ -1,9 +1,17 @@
 package spellpad.eventhandlers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextArea;
-import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooserBuilder;
+import javafx.stage.PopupWindow;
 
 /**
  *
@@ -18,9 +26,35 @@ public class SaveEventHandler implements EventHandler<ActionEvent> {
     }
     
     @Override
-    public void handle(ActionEvent arg0) {
-        
-       
+    public void handle(ActionEvent t) {
+        if(t == null || textAreaBound == null)return;
+         //Build a FileChooser
+        FileChooser fileChooser = buildFileChooser();
+        //Display the FileChooser and retrieve chosen path
+        File chosenFile = fileChooser.showSaveDialog(new PopupWindow() {});
+        //Check file path
+        if(chosenFile == null){
+            System.out.println("Save aborted");
+            return;
+        }
+        String fileContents = textAreaBound.getText();
+        try{
+            FileOutputStream fileOS = new FileOutputStream(chosenFile);
+            PrintWriter writer = new PrintWriter(fileOS);
+            writer.print(fileContents);
+            writer.flush();
+            writer.close();
+        }catch(IOException ex){
+            Logger.getLogger(SaveEventHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private FileChooser buildFileChooser() {
+        FileChooserBuilder fileChooserBuilder = FileChooserBuilder.create();
+        fileChooserBuilder.initialDirectory(new File("C:/"));
+        fileChooserBuilder.extensionFilters(new FileChooser.ExtensionFilter("Text Files", "*.txt", "*.spellpad"));
+        fileChooserBuilder.title("Save the text file");
+        return fileChooserBuilder.build();
     }
     
 }
