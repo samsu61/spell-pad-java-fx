93bcd10e5a570f53a20a2b1a8b384345d7cbe9d4
Jesse <Jesse@Work-PC>
2012-04-11 10:18:26 -0600

    Day 3, Part 1: Added a skeleton Swing project for the checkpoint requirements.

---
+++ SpellPad Swing Edition/src/spellpadswing/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spellpadswing;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jesse
+ */
+public class Main {
+
+   static public void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setBounds(0, 0, 200, 300);
+        window.setVisible(true);
+        
+    }
+}
