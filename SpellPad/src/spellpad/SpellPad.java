575c4083f1e571f9ac839e5e89d046058ec92f92
Jesse <Jesse@Work-PC>
2012-04-11 09:55:12 -0600

    Day 3, Part 1: Added a switch Editor button to swap from plain-text to rich text. Does not work, but code exists.

--- SpellPad/src/spellpad/SpellPad.java
+++ SpellPad/src/spellpad/SpellPad.java
@@ -3,16 +3,19 @@ package spellpad;
 import javafx.application.Application;
 import javafx.scene.CacheHint;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
-import spellpad.eventhandlers.OnKeyTyped;
 import spellpad.eventhandlers.OpenEventHandler;
 import spellpad.eventhandlers.SaveEventHandler;
+import spellpad.eventhandlers.SwitchEditorEventHandler;
 import spellpad.testing.UnitTests;
 
 /**
@@ -22,53 +25,61 @@ import spellpad.testing.UnitTests;
 public class SpellPad extends Application {
 
     boolean runTests = false;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        if(runTests){
+        if (runTests) {
             boolean allTestsPass = UnitTests.RunTests();
             System.out.println("All tests pass: " + allTestsPass);
         }
-        
-        
+
+
         //Build root pane
         BorderPane root = new BorderPane();
-        
+
         //Build Menubar
         MenuBar menuBar = new MenuBar();
         Menu file = new Menu("File");
+        Menu editor = new Menu("Editor");
+        MenuItem switchEditor = new MenuItem("Switch Editor");
+        switchEditor.setAccelerator(new KeyCodeCombination(
+                KeyCode.F4));
+        editor.getItems().add(switchEditor);
+
         MenuItem openItem = new MenuItem("Open");
         MenuItem saveItem = new MenuItem("Save");
-        
+
         KeyCodeCombination controlO = new KeyCodeCombination(
-                KeyCode.O, 
+                KeyCode.O,
                 KeyCombination.CONTROL_DOWN);
         openItem.setAccelerator(controlO);
-        
+
         KeyCodeCombination controlS = new KeyCodeCombination(
                 KeyCode.S,
                 KeyCombination.CONTROL_DOWN);
         saveItem.setAccelerator(controlS);
-        
+
         HTMLEditor texty = new HTMLEditor();
         //texty.setOnKeyTyped(new OnKeyTyped());
         texty.setCacheHint(CacheHint.SPEED);
         //texty.getOnKeyTyped();
         //texty.setWrapText(true);
-        
+
         openItem.setOnAction(new OpenEventHandler(texty));
         saveItem.setOnAction(new SaveEventHandler(texty));
-                
+
         file.getItems().add(openItem);
         file.getItems().add(saveItem);
         menuBar.getMenus().add(file);
+        menuBar.getMenus().add(editor);
         primaryStage.setTitle("Spell Pad");
         TreeView tree = new TreeView();
         tree.setPrefWidth(150.0);
@@ -77,9 +88,7 @@ public class SpellPad extends Application {
         root.setLeft(tree);
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
-        
-        Test t = new Test();
-        System.out.println(t.GetUserAgentStyleSheet());
-        
+        switchEditor.setOnAction(new SwitchEditorEventHandler(primaryStage.getScene()));
+
     }
 }
