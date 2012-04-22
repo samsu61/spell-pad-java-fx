287c1b2008bad501ef82f6822bf8425e210728b0
Jesse Allen <Jesse Allen@ELIZABETH>
2012-04-22 10:55:02 -0600

    Week 2, Weekend: All tests pass. I am excited. That's why that sentence ended with a period.

--- SpellPadSwing/src/spellpad/swing/SpellPadEditorPane.java
+++ SpellPadSwing/src/spellpad/swing/SpellPadEditorPane.java
@@ -8,45 +8,4 @@ import javax.swing.JTextPane;
  */
 public class SpellPadEditorPane extends JTextPane {
 
-    String bodyTag = "<body>";
-
-    /**
-     * Seeks to the index that would be the true index if Html tags were not
-     * present.
-     *
-     * @param index the index ignoring html that we seek to
-     * @return the index in the actual string
-     */
-    @Deprecated
-    public int seekIgnoreHtml(int index) {
-        String content = getText();
-
-        int realIndex = content.indexOf(bodyTag);
-        realIndex = realIndex + bodyTag.length();
-        char[] realContent = content.toCharArray();
-        boolean inTag = false;
-        boolean lastWasSpace = false;
-        for (int i = realIndex; i < realContent.length && index >= 0; i++) {
-            if (lastWasSpace) {
-                if (realContent[i] == ' ') {
-                    realIndex++;
-                    continue;
-                }
-            } else if (realContent[i] == '<') {
-                inTag = true;
-                continue;
-            } else if (realContent[i] == '>') {
-                inTag = false;
-                continue;
-            } else if (inTag) {
-                continue;
-            } else if (realContent[i] == ' ') {
-                lastWasSpace = true;
-            }
-            realIndex++;
-            index--;
-        }
-        return realIndex;
-
-    }
 }
