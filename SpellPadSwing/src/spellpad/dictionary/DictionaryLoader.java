860a391e494aab978031cce885c22e8f5bfa8120
Jesse Allen <Jesse Allen@ELIZABETH>
2012-04-24 10:25:00 -0600

    Week 3, Day 2: working on loading dictionary.

---
+++ SpellPadSwing/src/spellpad/dictionary/DictionaryLoader.java
@@ -0,0 +1,43 @@
+package spellpad.dictionary;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import spellpad.swing.autocomplete.TernarySearchTree;
+import spellpad.swing.autocomplete.WordCountCache;
+
+/**
+ *
+ * @author Jesse Allen
+ */
+public class DictionaryLoader implements Runnable {
+
+    private File dictionary = new File("dictionary.dat");
+    private TernarySearchTree tree = new TernarySearchTree();
+    private boolean executionComplete = false;
+
+    @Override
+    public void run() {
+        try(Scanner input = new Scanner(new FileInputStream(dictionary))){
+            while(input.hasNext()){
+                tree.add(input.nextLine());
+            }
+        }catch(IOException ex){
+            Logger.getLogger(DictionaryLoader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        executionComplete = true;
+    }
+
+    public boolean isExecutionComplete() {
+        return executionComplete;
+    }
+
+    public TernarySearchTree getTree() {
+        return tree;
+    }
+    
+    
+}
